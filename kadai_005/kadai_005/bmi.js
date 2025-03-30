b4931caea0d5bf0c671e88c677deeb1a72edf3d5
sakusakupandatabetai <sakurayokoo49@gmail.com>
2025-03-30 16:32:54 +0900

    Add files via upload

---
+++ kadai_005/kadai_005/bmi.js
@@ -0,0 +1,25 @@
+//変数の宣言
+let weightName;
+
+//値の代入
+weightName = '体重';
+
+//変数の宣言・代入
+let weightNumber = 68;
+
+// コンソールへの出力
+console.log(weightName);
+console.log(weightNumber);
+
+let heightName;
+
+heightName = '身長';
+
+let heightNumber = 1.7;
+
+console.log(heightName);
+console.log(heightNumber);
+
+console.log(weightNumber / (heightNumber * heightNumber));
+
+
