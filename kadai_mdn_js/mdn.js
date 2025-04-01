9dae29f4a99b7b1fb5fbf4887d5d5ab526a24ea2
sakusakupandatabetai <sakurayokoo49@gmail.com>
2025-04-01 22:43:50 +0900

    Add files via upload

--- kadai_mdn_js/mdn.js
+++ kadai_mdn_js/mdn.js
@@ -1,5 +1,10 @@
-const newDate = () => {
-  console.log('2024年10月12日');
+const datenow = () => {
+  const today = new Date();
+  const year = today.getFullYear();
+  const month = today.getMonth() + 1;
+  const day = today.getDate();
+  
+  console.log(`${year}年${month}月${day}日`);
 }
 
-newDate();
+datenow();
